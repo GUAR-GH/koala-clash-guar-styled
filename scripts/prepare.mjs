b49f01ff0f2b30d901702e30b545d42bebf40205
coolcoala <pashokrulit@yandex.ru>
2026-03-05 10:28:58 +0300

    changed build for mihomo alpha

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -26,12 +26,12 @@ const MIHOMO_ALPHA_URL_PREFIX = `https://github.com/MetaCubeX/mihomo/releases/do
 let MIHOMO_ALPHA_VERSION
 
 const MIHOMO_ALPHA_MAP = {
-  'win32-x64': 'mihomo-windows-amd64-v3',
+  'win32-x64': 'mihomo-windows-amd64-v1',
   'win32-ia32': 'mihomo-windows-386',
   'win32-arm64': 'mihomo-windows-arm64',
-  'darwin-x64': 'mihomo-darwin-amd64-v3',
+  'darwin-x64': 'mihomo-darwin-amd64-v1',
   'darwin-arm64': 'mihomo-darwin-arm64',
-  'linux-x64': 'mihomo-linux-amd64-v3',
+  'linux-x64': 'mihomo-linux-amd64-v1',
   'linux-arm64': 'mihomo-linux-arm64'
 }
 
